5b9a0fdca736fe5267939fc1d9b0fd301dde4753
Your Name <you@example.com>
2022-06-19 20:31:32 -0400

    Create calculate entire wealth function using .filter()

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,6 +50,23 @@ function sortByRichest() {
     updateDOM();
 };
 
+// Filter only Millionaires
+function showMillionaires() {
+    data = data.filter(person => person.money > 1000000);
+
+    updateDOM();
+}
+
+// Calculates all users total wealth
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+    const wealthElement = document.createElement("div");
+    wealthElement.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthElement);
+
+};
+
 // Update DOM
 function updateDOM(providedData = data) {
     // Clear Main Div
@@ -75,3 +92,5 @@ function formatMoney(number) {
 addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
 sortBtn.addEventListener("click", sortByRichest);
+showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
